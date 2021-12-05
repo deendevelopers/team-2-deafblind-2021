aa455ac1888b56490cd20284323a1ac28234e89e
m-fidow <muna.a.fidow@gmail.com>
2021-12-05 16:34:57 +0000

    text added

--- Practice/src/components/recipe-article/RecipeArticle.js
+++ Practice/src/components/recipe-article/RecipeArticle.js
@@ -135,13 +135,10 @@ const RecipeArticle = ({
             fontSize={"2xl"}
             fontFamily={"body"}
           >
-            Boost your conversion rate
+            {title}
           </Heading>
           <Text color={"gray.500"}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
+            <p>{summary.replace(/(<([^>]+)>)/gi, "")}</p>
           </Text>
         </Stack>
         {/* <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
