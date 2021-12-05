4a32aababba5226cc31301dbc33f7a08f84fd042
m-fidow <muna.a.fidow@gmail.com>
2021-12-05 10:16:40 +0000

    changed fetSearchterm to setSearch term

--- Practice/src/pages/recipe-search/RecipeSearchPage.js
+++ Practice/src/pages/recipe-search/RecipeSearchPage.js
@@ -6,23 +6,25 @@ import axios from "axios";
 const API_KEY = "1fae167ef83a4a56b244effeb1a158ec";
 
 const RecipeSearchPage = () => {
-    const [ searchTerm, setSearchTerm ] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
-        const fetchRecipes = async (ingredient) => {
-            const response = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY}&ingredients=apples`);
-            const jsonResponse = await response.json();
-            console.log(jsonResponse);
-        }
-        fetchRecipes(searchTerm);
-    }, [searchTerm]);
+  useEffect(() => {
+    const fetchRecipes = async (ingredient) => {
+      const response = await axios.get(
+        `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY}&ingredients=apples`
+      );
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+    };
+    setSearchTerm(searchTerm);
+  }, [searchTerm]);
 
-    return (
-        <section>
-            <RecipeSearch searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-            <article></article>
-        </section>
-    )
-}
+  return (
+    <section>
+      <RecipeSearch searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <article></article>
+    </section>
+  );
+};
 
 export default RecipeSearchPage;
