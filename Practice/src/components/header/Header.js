f6e4879cc335ce39ca7dd88924fc39122854cf38
m-fidow <muna.a.fidow@gmail.com>
2021-12-05 14:45:03 +0000

    changed heading msg

--- Practice/src/components/header/Header.js
+++ Practice/src/components/header/Header.js
@@ -1,11 +1,14 @@
 import "./Header.scss";
+import { Heading } from "@chakra-ui/layout";
 
 const Header = ({ title, message, userName }) => (
   <header>
-    <h1>
+    <Heading as="h1" size="xl">
       {title} {userName}
-    </h1>
-    <p>{message}</p>
+    </Heading>
+    <Heading as="h2" size="md">
+      {message}
+    </Heading>
   </header>
 );
 
--- Practice/src/pages/home/HomePage.js
+++ Practice/src/pages/home/HomePage.js
@@ -6,15 +6,19 @@ import RandomRecipePage from "../random-recipe/RandomRecipePage";
 import RecipeSearchPage from "../recipe-search/RecipeSearchPage";
 
 const HomePage = ({ history }) => (
-    <React.Fragment>
-        <Header title="Welcome to the Recipe Mate" message="We hope you have an amazing cooking experience together!" />
-        <RecipeSearchPage />
-        <section className="homepage-section">
-            <h2>Click here to go to be a bit adventurous and search for a random recipe!</h2>
-            <RandomRecipePage />
-        </section>
-    </ React.Fragment>
-)
-
+  <React.Fragment>
+    <Header
+      title="Search it, dish it, plate it"
+      message="Everyone needs a Recipe Mate"
+    />
+    <RecipeSearchPage />
+    {/* <section className="homepage-section">
+      <h2>
+        Click here to go to be a bit adventurous and search for a random recipe!
+      </h2>
+      <RandomRecipePage />
+    </section> */}
+  </React.Fragment>
+);
 
 export default HomePage;
