71b748cd789f53f5cd891c8dce300cd0d1442c6f
quantumomid <omid.wakili@outlook.com>
2021-12-10 15:38:56 +0000

    some further refactoring to avoid repition and reuse components

--- Practice/src/components/advanced-search/AdvancedSearch.js
+++ Practice/src/components/advanced-search/AdvancedSearch.js
@@ -1,90 +1,26 @@
 import React from "react";
 import ChakraCustomButton from "../chakra-custom-button/ChakraCustomButton";
-import {
-    Box,
-    Flex,
-    FormControl,
-    FormLabel,
-    RadioGroup,
-    Radio
-  } from '@chakra-ui/react';
+import { Box, Flex, FormControl, FormLabel, RadioGroup, Radio } from '@chakra-ui/react';
 import { ArrowUpIcon } from "@chakra-ui/icons";
+import RadioInputs from "./RadioInputs";
 
 const diets = ["vegetarian", "vegan", "gluten free", "ketogenic", "pescetarian",];
-
-const allergies = ["dairy", "shellfish", "peanut"];
-
+const allergies = ["dairy", "shellfish", "peanut",];
 const mealTypes = [ "main course", "dessert", "appetizer", "salad", "breakfast", "soup", "snack",];
 
 const AdvancedSearch = ({ dietTerm, allergiesTerm, mealTypesTerm, setDietTerm, setAllergiesTerm, setMealTypesTerm, setShowAdvancedSearch }) => (
     <Box bg="white" marginTop={2} p={3}>
-    <form>
+        <form>
+            <RadioInputs setter={setDietTerm} groupLabelState={dietTerm} groupLabel="Diet" radioLabels={diets} />
 
-        <FormControl as="fieldset">
-            <FormLabel as="legend">Diet</FormLabel>
-            <RadioGroup>
-                <Flex direction="column">
-                {diets.map((diet) => (
-                    <Radio 
-                        key={diet} 
-                        id={diet}
-                        name={diet}
-                        value={diet}
-                        checked={dietTerm === diet}
-                        onChange={() => setDietTerm(diet)}
-                    >
-                        {diet}
-                    </Radio>
-                ))}
-                </Flex>
-            </RadioGroup>
-        </FormControl>
+            <RadioInputs setter={setAllergiesTerm} groupLabelState={allergiesTerm} groupLabel="Allergies" radioLabels={allergies} />
 
-        <FormControl as="fieldset">
-            <FormLabel as="legend">Allergies</FormLabel>
-            <RadioGroup>
-                <Flex direction="column">
-                {allergies.map((allergy) => (
-                    <Radio 
-                        key={allergy} 
-                        id={allergy}
-                        name={allergy}
-                        value={allergy}
-                        checked={allergiesTerm === allergy}
-                        onChange={() => setAllergiesTerm(allergy)}
-                    >
-                        {allergy}
-                    </Radio>
-                ))}
-                </Flex>
-            </RadioGroup>
-        </FormControl>
-
-        <FormControl as="fieldset">
-            <FormLabel as="legend">Meal Types</FormLabel>
-            <RadioGroup>
-                <Flex direction="column">
-                {mealTypes.map((mealType) => (
-                    <Radio 
-                        key={mealType} 
-                        id={mealType}
-                        name={mealType}
-                        value={mealType}
-                        isChecked={mealTypesTerm === mealType}
-                        onChange={() => setMealTypesTerm(mealType)}
-                    >
-                        {mealType}
-                    </Radio>
-                ))}
-                </Flex>
-            </RadioGroup>
-        </FormControl>
-
-    </form>
-    <ChakraCustomButton onClick={() => setShowAdvancedSearch(false)}>
-      Back to top <ArrowUpIcon marginLeft={2} border="2px" borderRadius={50} name="arrow-up" />
-    </ChakraCustomButton>
-  </Box>
+            <RadioInputs setter={setMealTypesTerm} groupLabelState={mealTypesTerm} groupLabel="Meal Types" radioLabels={mealTypes} />
+        </form>
+        <ChakraCustomButton onClick={() => setShowAdvancedSearch(false)}>
+            Back to top <ArrowUpIcon marginLeft={2} border="2px" borderRadius={50} name="arrow-up" />
+        </ChakraCustomButton>
+    </Box>
 )
 
 export default AdvancedSearch;
---
+++ Practice/src/components/advanced-search/RadioInputs.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Box, Flex, FormControl, FormLabel, RadioGroup, Radio } from '@chakra-ui/react';
+
+const RadioInputs = ({ groupLabel, radioLabels, setter, groupLabelState }) => (
+    <FormControl as="fieldset">
+        <FormLabel as="legend">{groupLabel}</FormLabel>
+        <RadioGroup>
+            <Flex direction="column">
+            {radioLabels.map((type) => (
+                <Radio 
+                    key={type} 
+                    id={type}
+                    name={type}
+                    value={type}
+                    isChecked={groupLabelState === type}
+                    onChange={() => setter(type)}
+                >
+                    {type}
+                </Radio>
+            ))}
+            </Flex>
+        </RadioGroup>
+    </FormControl>  
+)
+
+export default RadioInputs;
